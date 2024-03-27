9cf56c74ecd733955d48a8bf9d5fb6bde251f40a
monircom <monircom@gmail.com>
2024-03-27 21:26:49 +0600

    Sorting button Added

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -13,13 +13,19 @@ const List = () => {
       </div>
 
       <div className="flex justify-center mt-8 mb-14">
-        <select className="select w-full max-w-xs bg-[#23BE0A] text-white text-xl font-semibold">
-            <option disabled selected>Sort By</option>
-            <option>Rating</option>
-            <option>Number of Pages</option>
-            <option>Publisher Year</option>            
-        </select>
+       
+        <div className="dropdown dropdown-bottom dropdown-end">
+            <div tabIndex={0} role="button" className="btn w-full m-1 bg-[#23BE0A] text-white text-xl font-semibold px-12">Sort By &nbsp; <img src="./images/down.png" alt="" /></div>
+            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                <li><a>Rating</a></li>
+                <li><a>Number of Pages</a></li>
+                <li><a>Publisher Year</a></li>
+            </ul>
+            </div>
       </div>
+
+
+
 
       <div>
       <Tabs>
