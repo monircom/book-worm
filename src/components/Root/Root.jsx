2ca6b203dbbe6ec6ab4279fd68608d749b8dc656
monircom <monircom@gmail.com>
2024-03-27 12:40:15 +0600

    Font Added NavBar Added

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
+import Navbar from '../Header/Navbar';
 
 const Root = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
