58752956df3da7f055ca1f4e9bc7ed01f8bdc6ae
monircom <monircom@gmail.com>
2024-03-27 13:13:27 +0600

    Error page Added

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
         <div>
             <p>Error Found</p>
+            <Link to='/'><button>Home</button></Link>
         </div>
     );
 };
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
             {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">Book Worm</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
@@ -56,7 +56,8 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn">Sign In</a>
+        <a className="btn">Sign Up</a>
       </div>
     </div>
   );
