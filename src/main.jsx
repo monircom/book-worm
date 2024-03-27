125c60c83b588a2e74bbb2b366be725f34e41953
monircom <monircom@gmail.com>
2024-03-27 12:13:41 +0600

    Testing React Route

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from './components/Root/Root';
+import Home from './components/Home/Home';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children:[
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
   },
 ]);
 
